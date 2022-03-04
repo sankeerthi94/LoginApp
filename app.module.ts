1eedbd1bc088ffa2dad4ebe3662c374c01811462
Sankeerthi Ganni <77227436+sankeerthi94@users.noreply.github.com>
2022-03-04 18:01:03 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {from} from 'rxjs';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +11,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
